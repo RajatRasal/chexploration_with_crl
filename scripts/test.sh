bc06c5f2887b7df7eeecd612641c1a43402fbfb0
Rajat Rasal <yugiohrajat1@gmail.com>
2025-02-11 02:18:34 +0000

    bug with invariant training

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-./scripts/train_job.sh 1 chexpert chexpert '0 1' '2' 0 1
-./scripts/train_job.sh 1 chexpert chexpert '0 2' '1' 0 1
-./scripts/train_job.sh 1 chexpert chexpert '2 1' '0' 0 1
-
-./scripts/train_job.sh 2 chexpert chexpert '0 1' '2' 1 1
+# ./scripts/train_job.sh 1 chexpert chexpert '0 1' '2' 0 1
+# ./scripts/train_job.sh 1 chexpert chexpert '0 2' '1' 0 1
+# ./scripts/train_job.sh 1 chexpert chexpert '2 1' '0' 0 1
+# 
+# ./scripts/train_job.sh 2 chexpert chexpert '0 1' '2' 1 1
 ./scripts/train_job.sh 2 chexpert chexpert '0 2' '1' 1 1
 ./scripts/train_job.sh 2 chexpert chexpert '2 1' '0' 1 1
