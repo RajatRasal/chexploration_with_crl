f693ae83fcad32e11b3909c031758cc7ba2d513f
Rajat Rasal <yugiohrajat1@gmail.com>
2025-02-12 12:18:49 +0000

    update race invariance slurm jobs

--- scripts/race_invariance.sh
+++ scripts/race_invariance.sh
@@ -1,42 +1,52 @@
 #!/bin/bash
 
-# ####### Densenet
-# # Dataset transfer exps -- normal
-# sbatch ./scripts/train_job.sh 1 chexpert mimic '0 1 2' '0 1 2' 42 densenet 0 0
-# sbatch ./scripts/train_job.sh 1 mimic chexpert '0 1 2' '0 1 2' 42 densenet 0 0
+##### Resnet
+# Dataset transfer exps -- normal
+sbatch ./scripts/train_job.sh 2 chexpert mimic '0 1 2' '0 1 2' 42 resnet 0 0
+sbatch ./scripts/train_job.sh 2 mimic chexpert '0 1 2' '0 1 2' 42 resnet 0 0
 
-# # Dataset transfer exps -- invariant
-# sbatch ./scripts/train_job.sh 2 chexpert mimic '0 1 2' '0 1 2' 42 densenet 1 0
-# sbatch ./scripts/train_job.sh 2 mimic chexpert '0 1 2' '0 1 2' 42 densenet 1 0
+# Attribute transfer exps -- normal
+sbatch ./scripts/train_job.sh 1 chexpert chexpert '0 1' '2' 42 resnet 0 0
+sbatch ./scripts/train_job.sh 1 chexpert chexpert '0 2' '1' 42 resnet 0 0
+sbatch ./scripts/train_job.sh 1 chexpert chexpert '2 1' '0' 42 resnet 0 0
 
-# # Attribute transfer exps -- normal
-# sbatch ./scripts/train_job.sh 1 chexpert chexpert '0 1' '2' 42 densenet 0 0
-# sbatch ./scripts/train_job.sh 1 chexpert chexpert '0 2' '1' 42 densenet 0 0
-# sbatch ./scripts/train_job.sh 1 chexpert chexpert '2 1' '0' 42 densenet 0 0
+##### Densenet
+# Dataset transfer exps -- normal
+sbatch ./scripts/train_job.sh 1 chexpert mimic '0 1 2' '0 1 2' 42 densenet 0 0
+sbatch ./scripts/train_job.sh 1 mimic chexpert '0 1 2' '0 1 2' 42 densenet 0 0
 
-# # Attribute transfer exps -- invariant
-# sbatch ./scripts/train_job.sh 2 chexpert chexpert '0 1' '2' 42 densenet 1 0
-# sbatch ./scripts/train_job.sh 2 chexpert chexpert '0 2' '1' 42 densenet 1 0
-# sbatch ./scripts/train_job.sh 2 chexpert chexpert '2 1' '0' 42 densenet 1 0
+# Dataset transfer exps -- invariant
+sbatch ./scripts/train_job.sh 2 chexpert mimic '0 1 2' '0 1 2' 42 densenet 1 0
+sbatch ./scripts/train_job.sh 2 mimic chexpert '0 1 2' '0 1 2' 42 densenet 1 0
 
-# ####### ViTB16
-# # Dataset transfer exps -- normal
-# sbatch ./scripts/train_job.sh 1 chexpert mimic '0 1 2' '0 1 2' 42 vitb16 0 0
-# sbatch ./scripts/train_job.sh 1 mimic chexpert '0 1 2' '0 1 2' 42 vitb16 0 0
+# Attribute transfer exps -- normal
+sbatch ./scripts/train_job.sh 1 chexpert chexpert '0 1' '2' 42 densenet 0 0
+sbatch ./scripts/train_job.sh 1 chexpert chexpert '0 2' '1' 42 densenet 0 0
+sbatch ./scripts/train_job.sh 1 chexpert chexpert '2 1' '0' 42 densenet 0 0
 
-# # Dataset transfer exps -- invariant
-# sbatch ./scripts/train_job.sh 2 chexpert mimic '0 1 2' '0 1 2' 42 vitb16 1 0
-# sbatch ./scripts/train_job.sh 2 mimic chexpert '0 1 2' '0 1 2' 42 vitb16 1 0
+# Attribute transfer exps -- invariant
+sbatch ./scripts/train_job.sh 2 chexpert chexpert '0 1' '2' 42 densenet 1 0
+sbatch ./scripts/train_job.sh 2 chexpert chexpert '0 2' '1' 42 densenet 1 0
+sbatch ./scripts/train_job.sh 2 chexpert chexpert '2 1' '0' 42 densenet 1 0
 
-# # Attribute transfer exps -- normal
-# sbatch ./scripts/train_job.sh 1 chexpert chexpert '0 1' '2' 42 vitb16 0 0
-# sbatch ./scripts/train_job.sh 1 chexpert chexpert '0 2' '1' 42 vitb16 0 0
-# sbatch ./scripts/train_job.sh 1 chexpert chexpert '2 1' '0' 42 vitb16 0 0
+####### ViTB16
+# Dataset transfer exps -- normal
+sbatch ./scripts/train_job.sh 1 chexpert mimic '0 1 2' '0 1 2' 42 vitb16 0 0
+sbatch ./scripts/train_job.sh 1 mimic chexpert '0 1 2' '0 1 2' 42 vitb16 0 0
 
-# # Attribute transfer exps -- invariant
-# sbatch ./scripts/train_job.sh 2 chexpert chexpert '0 1' '2' 42 vitb16 1 0
-# sbatch ./scripts/train_job.sh 2 chexpert chexpert '0 2' '1' 42 vitb16 1 0
-# sbatch ./scripts/train_job.sh 2 chexpert chexpert '2 1' '0' 42 vitb16 1 0
+# Dataset transfer exps -- invariant
+sbatch ./scripts/train_job.sh 2 chexpert mimic '0 1 2' '0 1 2' 42 vitb16 1 0
+sbatch ./scripts/train_job.sh 2 mimic chexpert '0 1 2' '0 1 2' 42 vitb16 1 0
+
+# Attribute transfer exps -- normal
+sbatch ./scripts/train_job.sh 1 chexpert chexpert '0 1' '2' 42 vitb16 0 0
+sbatch ./scripts/train_job.sh 1 chexpert chexpert '0 2' '1' 42 vitb16 0 0
+sbatch ./scripts/train_job.sh 1 chexpert chexpert '2 1' '0' 42 vitb16 0 0
+
+# Attribute transfer exps -- invariant
+sbatch ./scripts/train_job.sh 2 chexpert chexpert '0 1' '2' 42 vitb16 1 0
+sbatch ./scripts/train_job.sh 2 chexpert chexpert '0 2' '1' 42 vitb16 1 0
+sbatch ./scripts/train_job.sh 2 chexpert chexpert '2 1' '0' 42 vitb16 1 0
 
 ####### ResNet
 # MCC exps
